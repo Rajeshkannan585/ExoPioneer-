ff01a63124c4486ca33ea161260efc4ac92e7595
Rajeshkannan585 <rajeshkannan75823@gmail.com>
2025-10-14 12:30:37 +0530

    Create StoryData.cs

---
+++ Assets/Scripts/Story/StoryData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[System.Serializable]
+public class StoryData
+{
+    public string storyID;
+    public string title;
+    public string description;
+    public AudioClip voiceClip;
+    public bool isUnlocked;
+    public bool isPlayed;
+}
